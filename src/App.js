a44a8b3b1e073a23cbb45e6ede12ddf34559c66f
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-16 12:05:19 +0900

    checked ModuleIds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Hello from "./app/Hello";
+// import Hello from "./app/Hello";
 // import World from "./app/World";
 
 import './App.css';
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <Hello name="React" />
+        {/* <Hello name="React" /> */}
         {/* <World /> */}
       </div>
     );
